c93c1e0a8cdc024ce9007da6f77087bc3b276036
JonasMarx3007 <jonas.marx3007@gmx.de>
2025-08-14 13:47:01 +0200

    server_app

---
+++ app_server.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import sys
+import os
+from ui.i0_main_tabs import render_main_tabs
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+def main():
+    favicon_path = resource_path("favicon.ico")
+    st.set_page_config(page_title="Proteomics Copilot", layout="wide", page_icon=favicon_path)
+    st.title("Proteomics Copilot")
+    render_main_tabs()
+
+if __name__ == "__main__":
+    main()
---
+++ ui/i0_main_tabs_server.py
@@ -0,0 +1,24 @@
+import time
+from ui.i1_data_ui import *
+from ui.i2_qc_pipline_ui import *
+from ui.i3_statistical_analysis_ui import *
+from ui.i4_single_protein_ui import *
+from ui.i5_phospho_specific_ui import *
+
+def render_main_tabs():
+    main_tabs = st.tabs(["Data", "QC Pipeline", "Statistical Analysis", "Single Protein", "Phospho-specific"])
+
+    with main_tabs[0]:
+        data_ui()
+
+    with main_tabs[1]:
+        qc_pipeline_ui()
+
+    with main_tabs[2]:
+        statistical_analysis_ui()
+
+    with main_tabs[3]:
+        single_protein_ui()
+
+    with main_tabs[4]:
+        phospho_specific_ui()
